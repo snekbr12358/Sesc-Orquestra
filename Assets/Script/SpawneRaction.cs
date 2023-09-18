f7645a41aa3967ce1cf5609df5a08124b6cc7819
snekbr12358 <luizdaniell590@gmail.com>
2023-09-18 11:19:08 -0300

    new

--- Assets/Script/SpawneRaction.cs
+++ Assets/Script/SpawneRaction.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class SpawneRaction : MonoBehaviour
 {
-   
+    public float larguraw = 10;
+    public float alturah = 5;
     public List<GameObject> Notas;
     public float tempo = 0;
 
@@ -14,6 +15,11 @@ public class SpawneRaction : MonoBehaviour
         QuedaRandomizada();
 
 
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(larguraw, alturah, 0));
     }
 
 
@@ -41,9 +47,9 @@ public class SpawneRaction : MonoBehaviour
             tempo = 0;
             //RandomizarPosição
             float posX = Random.Range(-2, 2);
-            Vector3 novaPos = new Vector3(posX, 6, 0);
+            Vector3 novaPos = new Vector3(posX, 13, 0);
             //Randomizar o Objeto
-            int probabilidade = Random.Range(0, 100);
+            int probabilidade = Random.Range(0, 106);
             if (probabilidade < 12)
             {
                 GameObject novaNota = Instantiate(Notas[0], novaPos, Quaternion.identity);
@@ -58,6 +64,47 @@ public class SpawneRaction : MonoBehaviour
                 GameObject novaNota = Instantiate(Notas[2], novaPos, Quaternion.identity);
                 Destroy(novaNota,  2f);
             }
+            else if (probabilidade < 48)
+            {
+                GameObject novaBolinha = Instantiate(Notas[3], novaPos, Quaternion.identity);
+                Destroy(novaBolinha, 2f);
+            }
+            else if (probabilidade < 55)
+            {
+                GameObject novaBolinha = Instantiate(Notas[4], novaPos, Quaternion.identity);
+                Destroy(novaBolinha, 2f);
+            }
+            else if (probabilidade < 75)
+            {
+                GameObject novaBolinha = Instantiate(Notas[5], novaPos, Quaternion.identity);
+                Destroy(novaBolinha, 2f);
+            }
+            else if (probabilidade < 85)
+            {
+                GameObject novaBolinha = Instantiate(Notas[6], novaPos, Quaternion.identity);
+                Destroy(novaBolinha, 2f);
+            }
+            else if (probabilidade < 90)
+            {
+                GameObject novaBolinha = Instantiate(Notas[7], novaPos, Quaternion.identity);
+                Destroy(novaBolinha, 2f);
+            }
+            else if(probabilidade < 95)
+            {
+                GameObject novaBolinha = Instantiate(Notas[8], novaPos, Quaternion.identity);
+                Destroy(novaBolinha, 2f);
+            }
+            else if (probabilidade < 100)
+            {
+                GameObject novaBolinha = Instantiate(Notas[9], novaPos, Quaternion.identity);
+                Destroy(novaBolinha, 2f);
+            }
+            else if (probabilidade < 105)
+            {
+                GameObject novaBolinha = Instantiate(Notas[10], novaPos, Quaternion.identity);
+                Destroy(novaBolinha, 2f);
+            }
+            
 
 
 
