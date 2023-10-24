9e7547556e71cc3d1d3cd740159c568ea0cd3cd8
snekbr12358 <luizdaniell590@gmail.com>
2023-10-24 07:27:11 -0300

    new

--- Assets/Script/Loja.cs
+++ Assets/Script/Loja.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,7 +8,7 @@ public class Loja : MonoBehaviour
 {
     //Variavel Texto de Valor de Coração
 
-    public Text valorVida;
+    public TextMeshProUGUI valorVida;
 
     //Informação do Banco
 
@@ -16,12 +17,27 @@ public class Loja : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+      MeuBanco = GameObject.FindGameObjectWithTag("GameController").GetComponent<Banco>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    //Realizar Comprar
+    public void ComprarVida()
+    {
+        int vidascompradas = PlayerPrefs.GetInt("nivelVida") + 1;
+        int custo = (vidascompradas * 10);
+        if (MeuBanco.Pagar(custo) == true)
+        {
+            //conseguiu compra
+            PlayerPrefs.SetInt("nivelVida", vidascompradas);
+        }
+        else
+        {
+            //não conseguiu comprar
+        }
     }
 }
